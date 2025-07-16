19b0f7119be2a74f35e42b2f49a65e7592b359bf
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-07-16 19:53:36 +0530

    feat: Setup assets build & enqueue assets

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Mega Menus Theme functions and definitions
+ *
+ * @package megamenus-theme
+ */
+
+// Enqueue scripts and styles for navigation menus.
+add_action( 'wp_enqueue_scripts', 'megamenu_enqueue_scripts' );
+add_action( 'admin_enqueue_scripts', 'megamenu_enqueue_scripts' );
+
+/**
+ * Enqueue scripts and styles for the theme.
+ */
+function megamenu_enqueue_scripts() {
+
+	wp_enqueue_script(
+		'megamenu-script',
+		get_template_directory_uri() . '/build/index.js',
+		array( 'jquery' ),
+		'1.0.0',
+		false
+	);
+
+	wp_enqueue_style(
+		'megamenu-style',
+		get_template_directory_uri() . '/build/index.css',
+		array(),
+		'1.0.0',
+	);
+}
